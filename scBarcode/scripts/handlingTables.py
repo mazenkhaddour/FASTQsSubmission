82b08f99636039f730cdf312a05c6b10811b9c51
mazen.k <mazen.khaddour@fht.org>
2026-04-22 11:53:58 +0200

    scBarcode

--- scBarcode/scripts/handlingTables.py
+++ scBarcode/scripts/handlingTables.py
@@ -9,14 +9,14 @@ parser.add_argument('-o', '--output', help='Output directory', required=True)
 args = parser.parse_args()
 
 
-CellIDs = pd.read_csv(args.input , sep="\t" )
-Singlet = CellIDs.query("DropletType == 'Singlet'")  # Filter the CellIDs DataFrame to include only rows where DropletType is 'Singlet'
-Singlet_barcodes = Singlet.loc[:,["barcode","FirstID"]]
-#creat folder to store 
-if os.path.exists(args.output+"/barcodes") == False:
-    os.makedirs(args.output+"/barcodes")
-for genotype in Singlet_barcodes.FirstID.unique():
+
+CellIDs_original = pd.read_csv(args.input , sep="\t" )
+ine_dict = {"KOLF2C1day25": "CTL08A", "S20201":"CTL04E", "MIFF1day25":"CTL02A"}
+CellID = CellIDs_original.query("Consensus not in ['doublet' , 'LowQuality']")
+CellID["Consensus"] = CellID["Consensus"].replace(ine_dict)
+
+for genotype in CellID.Consensus.unique():
     #write index as txt file 
-    index = Singlet_barcodes.query("FirstID == @genotype").barcode.tolist()  # Get the list of barcodes for the current genotype
+    index = CellID.query("Consensus == @genotype").barcode.tolist()  # Get the list of barcodes for the current genotype
     with open(f"{args.output}/barcodes/{genotype}_barcodes.txt", "w") as f:
         f.write("\n".join(index))  # Write the barcodes to a text file, one per line    
